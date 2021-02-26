f8eca39a4e13a1062336339f9227dae98512bd15
Cassidy Bandy <31383109+c-bandy@users.noreply.github.com>
2021-02-26 16:45:27 -0700

    add vercel integration

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import * as sapper from '@sapper/server';
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-polka() // You can also use Express
+export default polka() // You can also use Express
 	.use(
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
