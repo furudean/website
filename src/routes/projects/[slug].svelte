67426aed39924866cf7c999e659940cae35ea30a
Cassidy Bandy <31383109+c-bandy@users.noreply.github.com>
2021-03-02 18:22:56 -0700

    remove unique link preview on projects
    adds too much complexity for not that much win. maybe there's a better way to do it?

--- src/routes/projects/[slug].svelte
+++ src/routes/projects/[slug].svelte
@@ -20,19 +20,7 @@
   import Icon from "../../components/Icon.svelte";
 
   export let project: Project;
-
-  const title = `${project.title} · Projects · Cassidy Bandy`;
 </script>
-
-<svelte:head>
-  <title>{title}</title>
-  <meta name="title" content={title} />
-  <meta name="description" content={project.summary} />
-  <meta property="og:title" content={title} />
-  <meta property="og:description" content={project.summary} />
-  <meta property="twitter:title" content={title} />
-  <meta property="twitter:description" content={project.summary} />
-</svelte:head>
 
 <nav aria-label="Breadcrumb">
   <a href="/projects">Projects</a> /
