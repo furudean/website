1607874ede1e08342fa99013ab15c26ca756442f
Cassidy Bandy <31383109+c-bandy@users.noreply.github.com>
2021-03-07 18:50:54 -0700

    update projects

--- src/routes/projects/_projects.ts
+++ src/routes/projects/_projects.ts
@@ -11,17 +11,18 @@ export interface Project {
 
 export const projects: Project[] = [
   {
-    title: "Portfolio website",
-    slug: "portfolio-site",
+    title: "Portfolio site",
+    slug: "portfolio-site-2021",
     date: "2021-02-26",
-    summary: "Personal portfolio website. You're looking at it!",
+    summary: "Updated portfolio for 2021.",
+    coverImageUrl: "https://cass.moe/media/projects/website/embed.jpg",
     repo: "https://github.com/c-bandy/website"
   },
   {
     title: "Type Kana",
     slug: "type-kana",
     date: "2020-08-02",
-    summary: "A quiz app to help you learn hiragana and katakana, the Japanese syllabaries",
+    summary: "A quiz app to help you learn hiragana and katakana, the Japanese syllabaries.",
     // TODO: cover image is hardcoded, find a better way here
     coverImageUrl: "https://cass.moe/media/projects/type-kana/session.jpg",
     link: "https://type-kana.cass.moe/",
